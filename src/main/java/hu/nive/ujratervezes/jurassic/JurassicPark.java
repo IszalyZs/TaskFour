74bdbde912a9fed5b54f6ae45f49ca603b94c665
IszalyZs <iszalyzs@gmail.com>
2021-04-29 11:16:45 +0200

    Add project

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -12,4 +16,21 @@ public class JurassicPark {
         this.dbPassword = dbPassword;
     }
 
+
+    public List<String> checkOverpopulation() {
+        List<String> dinos = new ArrayList<>();
+        String sql = "SELECT breed FROM dinosaur WHERE actual > expected ORDER BY breed ASC;";
+        try (Connection connection = DriverManager.getConnection(this.dbUrl, this.dbUser, this.dbPassword);
+             PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                dinos.add(resultSet.getString("breed"));
+            }
+            resultSet.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (dinos.size() == 0) return new ArrayList<>();
+        return dinos;
+    }
 }
